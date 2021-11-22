7ee35c53084e1b9f3d61a834ab250a02b43b7e0a
Nimeryon <t.auriol46@gmail.com>
2021-11-22 23:59:47 +0100

    Update footer.js

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -3,7 +3,7 @@ import { Toolbar, Box } from "@material-ui/core";
 // Components
 import CopyrightComponent from "./copyright.js";
 // Style
-import useStyles from "./styles/style.js";
+import useStyles from "../../styles/style.js";
 
 const Footer = () => {
     const classes = useStyles();
